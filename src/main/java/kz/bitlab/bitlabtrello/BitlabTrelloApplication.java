8cb88a3cb89093c04dbd1704abdd4274804a671e
peanutmutton <peanutmutton@outlook.com>
2025-03-08 23:53:34 +0500

    application.properties add log file

---
+++ src/main/java/kz/bitlab/bitlabtrello/BitlabTrelloApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.bitlabtrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlabTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitlabTrelloApplication.class, args);
+    }
+
+}
